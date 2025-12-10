1f6817363f52f07819b1cf2a688fb50b547b99f8
paulino <paulinoposada@gmail.com>
2025-12-10 21:40:07 +0100

    commit message

--- websites/paulino/miscellaneous/arduino/Arduino_ino_files/temp_sensor_DS18B20_multi/temp_sensor_DS18B20_multi.ino
+++ websites/paulino/miscellaneous/arduino/Arduino_ino_files/temp_sensor_DS18B20_multi/temp_sensor_DS18B20_multi.ino
@@ -6,7 +6,7 @@
 #include <DallasTemperature.h>
 
 // Data wire is plugged into port 3 on the Arduino
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS 9
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
